94615f0ddba63f9241e59580c999ffd6744a6289
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-09 13:19:45 -0700

    Making packages actually run and test and then also flow

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import endsWith from "./"
+import endsWith from "./source.js"
 
 test(({ok, end}) => {
   ok(endsWith("period")("I have no period"))
