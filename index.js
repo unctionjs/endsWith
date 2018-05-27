023e250bb04d275a58e2d4779306bb73f6393783
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:29 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import {test} from "ramda"
-import escapeStringRegexp from "escape-string-regexp"
-
-export default function endsWith (subset: string): Function {
-  const escaped = escapeStringRegexp(subset)
-  const ending = `${escaped}$`
-
-  return function endsWithSubset (set: string): boolean {
-    return test(new RegExp(ending))(set)
-  }
+import { test } from "ramda";
+import escapeStringRegexp from "escape-string-regexp";
+export default function endsWith(subset) {
+  const escaped = escapeStringRegexp(subset);
+  const ending = `${escaped}$`;
+  return function endsWithSubset(set) {
+    return test(new RegExp(ending))(set);
+  };
 }
